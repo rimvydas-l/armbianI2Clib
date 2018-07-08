438180fc5bdbfc2fa1f8ab39827c8b2826b6bcec
Rimvydas Lazdauskas <rimvydas.lazdauskas@gmail.com>
2018-07-08 11:51:26 +0300

    added solution and wrapper

---
+++ armbianI2Clib/LibcWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace armbianI2Clib
+{
+    public class LibcWrapper
+    {
+        [DllImport("libc.so", EntryPoint = "open")]
+        public static extern int Open(string fileName, int mode);
+
+        [DllImport("libc.so", EntryPoint = "ioctl", SetLastError = true)]
+        public static extern int Ioctl(int fd, int request, IntPtr data);
+
+        [DllImport("libc.so", EntryPoint = "read", SetLastError = true)]
+        public static extern int Read(int handle, byte[] data, int length);
+
+        [DllImport("libc.so", EntryPoint = "write", SetLastError = true)]
+        public static extern int Write(int handle, byte[] data, int length);
+
+    }
+}
