b7daf2025f56157f309e657c77d3ed53cfffef75
Rimvydas Lazdauskas <rimvydas.lazdauskas@gmail.com>
2018-07-10 19:02:43 +0300

    bugfix for lib and test sht31d

---
+++ TestSht31/Program.cs
@@ -0,0 +1,36 @@
+﻿using armbianI2Clib;
+using System;
+using System.Threading;
+
+namespace TestSht31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test of SHT31-D and armbianI2Clib");
+
+            try
+            {
+                using (var device = new I2CDevice("/dev/i2c-0", 0x44))
+                {
+                    Console.WriteLine("init complete");
+
+                    device.Write(new byte[] { 0x24, 0x00 }, 2);
+                    Thread.Sleep(100);
+                    Console.WriteLine("write complete");
+
+                    var readBuff = device.Read(3);
+
+                    Console.WriteLine($"Read count: {readBuff.Length}");
+                    Console.WriteLine($"Read 1:{readBuff[0]}, 2:{readBuff[1]}, 3:{readBuff[2]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            Console.WriteLine("END");
+        }
+    }
+}
--- armbianI2Clib/LibcWrapper.cs
+++ armbianI2Clib/LibcWrapper.cs
@@ -5,19 +5,19 @@ namespace armbianI2Clib
 {
     public class LibcWrapper
     {
-        [DllImport("libc.so", EntryPoint = "open")]
+        [DllImport("libc.so.6", EntryPoint = "open")]
         public static extern int Open(string fileName, int mode);
 
-        [DllImport("libc.so", EntryPoint = "ioctl", SetLastError = true)]
+        [DllImport("libc.so.6", EntryPoint = "ioctl", SetLastError = true)]
         public static extern int Ioctl(int fd, int request, int data);
 
-        [DllImport("libc.so", EntryPoint = "read", SetLastError = true)]
+        [DllImport("libc.so.6", EntryPoint = "read", SetLastError = true)]
         public static extern int Read(int handle, byte[] data, int length);
 
-        [DllImport("libc.so", EntryPoint = "write", SetLastError = true)]
+        [DllImport("libc.so.6", EntryPoint = "write", SetLastError = true)]
         public static extern int Write(int handle, byte[] data, int length);
 
-        [DllImport("libc.so", EntryPoint = "close", SetLastError = true)]
+        [DllImport("libc.so.6", EntryPoint = "close", SetLastError = true)]
         public static extern int Close(int fd);
     }
 }
